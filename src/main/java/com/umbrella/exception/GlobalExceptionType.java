ee130373027d3c0a5e1663fceafc204060266474
kimtaedong <rlaxoehd4234@naver.com>
2023-04-12 14:19:38 +0900

    [FEAT] PostService Add a Validation Logic in Global
    GlobalExceptionCode 추가

--- src/main/java/com/umbrella/exception/GlobalExceptionType.java
+++ src/main/java/com/umbrella/exception/GlobalExceptionType.java
@@ -1,3 +1,17 @@
 package com.umbrella.exception;
-public enum GlobalExceptionType {
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum GlobalExceptionType implements BaseExceptionType{
+    ;
+
+    private int errorCode;
+    private HttpStatus httpStatus;
+    private String errorMessage;
+
+    
 }
