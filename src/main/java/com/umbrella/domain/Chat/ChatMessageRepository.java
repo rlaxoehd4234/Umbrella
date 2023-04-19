eebc8e7d3cc15c0010346942cae8ba5906d7c62b
saul <dlathf3210@naver.com>
2023-04-19 13:07:44 +0900

    [feat] chat domain enhancement

--- src/main/java/com/umbrella/domain/Chat/ChatMessageRepository.java
+++ src/main/java/com/umbrella/domain/Chat/ChatMessageRepository.java
@@ -1,7 +1,9 @@
 package com.umbrella.domain.Chat;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -9,6 +11,8 @@ public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> 
 
     List<ChatMessage> findByChatRoom_ChatRoomName(String chatRoomName);
 
-    @Query
-    void deleteAllByChatRoom_ChatRoomName(String chatRoom); // 채팅방 삭제의 경우
+    @Modifying
+    @Query("DELETE FROM ChatMessage c WHERE c.chatRoom.chatRoomName = :chatRoomName")
+    void deleteAllByChatRoom_ChatRoomName(@Param("chatRoomName") String chatRoomName);
+    // 채팅방 삭제의 경우 채팅 기록 전부 삭제
 }
