00007382943125318b85753e172123dd7070d9fa
BullChallenger <am23am45@naver.com>
2023-06-02 20:47:46 +0900

    [FEAT] Workspace 입장 시 ResponseDTO 값에 Board 와 Event 값 추가

--- src/main/java/com/umbrella/dto/workspace/WorkspaceResponseDto.java
+++ src/main/java/com/umbrella/dto/workspace/WorkspaceResponseDto.java
@@ -2,6 +2,7 @@ package com.umbrella.dto.workspace;
 
 import com.umbrella.domain.WhenToMeet.Event;
 import com.umbrella.domain.WorkSpace.WorkSpace;
+import com.umbrella.dto.board.BoardResponseDto;
 import lombok.Data;
 import lombok.Getter;
 
@@ -14,11 +15,13 @@ public class WorkspaceResponseDto {
 
     private String title;
     private String description;
+    private List<BoardResponseDto> boards;
     private List<EventResponseDto> events;
 
     public WorkspaceResponseDto(WorkSpace workSpace){
         this.title = workSpace.getTitle();
         this.description = workSpace.getDescription();
+        this.boards = workSpace.getBoards().stream().map(BoardResponseDto::new).collect(Collectors.toList());
         this.events = workSpace.getEvents().stream().map(EventResponseDto::new).collect(Collectors.toList());
     }
 }
