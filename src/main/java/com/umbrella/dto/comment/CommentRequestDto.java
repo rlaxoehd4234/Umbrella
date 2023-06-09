56e4874a94be22186e288c106c847d628e9242ed
saul <dlathf3210@naver.com>
2023-06-09 14:54:27 +0900

    [FEAT]add DTO valid option

--- src/main/java/com/umbrella/dto/comment/CommentRequestDto.java
+++ src/main/java/com/umbrella/dto/comment/CommentRequestDto.java
@@ -3,13 +3,17 @@ package com.umbrella.dto.comment;
 import lombok.Getter;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Getter
 public class CommentRequestDto {
 
     @NotBlank
+    @Size(min = 1, max = 500, message = "1자 이상, 500자 이하만 가능합니다.")
     private String content;
-    @NotBlank
+
+    @NotNull
     private String nickName;
     @NotBlank
     private int pageNumber;
